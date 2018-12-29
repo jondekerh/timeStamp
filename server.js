8734eca761310bb1245fe9ee9e538a99e0df97da
Jon <jondekerh@gmail.com>
2018-12-29 14:36:01 -0800

    small change to startup message

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./app.js');
 var port = 3000;
 
 app.listen(port, () => {
-  console.log(`timeStamp listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
